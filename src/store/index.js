915c69ed7eba8531ae6db21340f8dbf3aa9d56c6
amkolotov <amkolotov@mail.ru>
2021-03-15 11:36:05 +0500

    docker changed

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,8 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
-        backendUrl: "http://127.0.0.1:80/api/v1"
+//        backendUrl: "http://127.0.0.1:1337/api/v1"
+        backendUrl: "http://5.63.157.5/api/v1"
     },
     mutations: {},
     actions: {},
